ae15cd2d2bb60e44117ca5e8abe20359814372a4
Lennox Scholl <ox0235198@gmail.com>
2022-09-25 22:17:23 -0400

    init

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
+        System.out.println("L       SSS");
+        System.out.println("L      S   S");
+        System.out.println("L      S");
+        System.out.println("L       SSS");
+        System.out.println("L          S");
+        System.out.println("L      S   S");
+        System.out.println("LLLLL   SSS");
         //print initials here. 
     }
 }
