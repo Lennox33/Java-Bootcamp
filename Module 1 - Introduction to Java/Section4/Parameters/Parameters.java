6c0cac9c1e62271eaab4651ca0160fbaaa65875f
Lennox Scholl <ox0235198@gmail.com>
2022-11-04 14:22:34 -0400

    init

--- Module 1 - Introduction to Java/Section4/Parameters/Parameters.java
+++ Module 1 - Introduction to Java/Section4/Parameters/Parameters.java
@@ -1,5 +1,10 @@
 public class Parameters {
     public static void main(String[] args) {
-        
+        calculateArea(2.3, 3.6);
     }
+
+public static void calculateArea (double width, double length){
+    double area = width * length;
+    System.out.println("Area " + area);
+}
 }
