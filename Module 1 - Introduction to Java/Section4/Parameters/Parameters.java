b7dc75fcaaf13cafcaf382d0249ab903a5074d15
Lennox Scholl <ox0235198@gmail.com>
2022-11-12 13:08:19 -0500

    init

--- Module 1 - Introduction to Java/Section4/Parameters/Parameters.java
+++ Module 1 - Introduction to Java/Section4/Parameters/Parameters.java
@@ -1,6 +1,8 @@
 public class Parameters {
     public static void main(String[] args) {
         calculateArea(2.3, 3.6);
+        calculateArea(1.9, 2.6);
+        calculateArea(3.8, 2.4);
     }
 
 public static void calculateArea (double width, double length){
--- Module 1 - Introduction to Java/Section4/Return/ReturnValues.java
+++ Module 1 - Introduction to Java/Section4/Return/ReturnValues.java
@@ -1,5 +1,13 @@
 public class ReturnValues {
     public static void main(String[] args) {
-        
+       double area1 =  calculateArea(2.3, 3.6);
+       double area2x =  calculateArea(1.9, 2.6);
+       double area3 = calculateArea(3.8, 2.4);
     }
+
+public static double calculateArea (double width, double length){
+    double area = width * length;
+    return area ;
+}
+
 }
