77911db5a768d5b6a8c2a425c8f5cdedc2bc8db7
Lennox Scholl <ox0235198@gmail.com>
2022-11-21 19:01:17 -0500

    init

--- Module 1 - Introduction to Java/Section4/Return/ReturnValues.java
+++ Module 1 - Introduction to Java/Section4/Return/ReturnValues.java
@@ -1,13 +1,29 @@
 public class ReturnValues {
     public static void main(String[] args) {
        double area1 =  calculateArea(2.3, 3.6);
-       double area2x =  calculateArea(1.9, 2.6);
-       double area3 = calculateArea(3.8, 2.4);
+      printArea(2.3, 3.6, area1);
+
+       String englishExplanation = explainArea("English");
+       System.out.println(englishExplanation);
     }
 
 public static double calculateArea (double width, double length){
+    if (length < 0 || width < 0){
+        System.out.println("INVALID DIMENSIONS");
+        System.exit(0);
+    }
     double area = width * length;
     return area ;
 }
 
+public static String explainArea(String language){
+    switch (language) {
+        case "English": return "Area equals length * width";
+        default: return "Language not available";
+    }
+}
+
+public static void printArea(double length, double width, double area){
+    System.out.println("A rectangle with the length of " + length + "and a width of " + width + " has an area of " + area);
+}
 }
